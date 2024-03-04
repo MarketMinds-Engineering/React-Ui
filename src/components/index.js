fc19b2cd0ae6223fe0f6dca7e5fd27e264e2e241
NitinSrivastava <nitin.srivastava@loram.com>
2024-03-04 14:27:14 +0530

    deleted all files

--- src/components/index.js
+++
@@ -1,8 +0,0 @@
-export { Button } from "./Button";
-export { Img } from "./Img";
-export { Input } from "./Input";
-export { Line } from "./Line";
-export { List } from "./List";
-export { SelectBox } from "./SelectBox";
-export { ReactTable } from "./Table";
-export { Text } from "./Text";
